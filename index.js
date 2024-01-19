287ef8df0087c5dd04a8220da3d636b96596992f
izone00 <woojin5555@gmail.com>
2024-01-19 15:38:29 +0900

    [Feat] todo 수정 및 삭제 구현

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ const storage = {
 
 const todoListUl = document.getElementById('todo-list');
 todoListUl.createTodoElemLi = function (todoItem) {
+  let isUpdating = false;
+
   const todoCheckbox = document.createElement('input');
   todoCheckbox.classList.add('todo-comlete-checkbox');
   todoCheckbox.type = 'checkbox';
@@ -46,11 +48,33 @@ todoListUl.createTodoElemLi = function (todoItem) {
   todoItem.completed && todoContent.classList.add('completed');
   todoContent.innerText = todoItem.content;
 
+  const todoUpdateButton = document.createElement('button');
+  todoUpdateButton.classList.add('todo-update-button');
+  todoUpdateButton.innerText = '수정';
+  todoUpdateButton.addEventListener('click', handleUpdateButton);
+
+  const todoDeleteButton = document.createElement('button');
+  todoDeleteButton.classList.add('todo-delete-button');
+  todoDeleteButton.innerText = '삭제';
+  todoDeleteButton.addEventListener('click', handleDeleteButton);
+
+  const todoSaveButton = document.createElement('button');
+  todoSaveButton.classList.add('todo-delete-button');
+  todoSaveButton.innerText = '저장';
+  todoSaveButton.addEventListener('click', handleSaveButton);
+
+  const todoCancelButton = document.createElement('button');
+  todoCancelButton.classList.add('todo-delete-button');
+  todoCancelButton.innerText = '취소';
+  todoCancelButton.addEventListener('click', handleCancelButton);
+
   const todoElemLi = document.createElement('li');
   function render() {
     todoElemLi.replaceChildren(
       todoCheckbox,
       todoContent,
+      isUpdating ? todoSaveButton : todoUpdateButton,
+      isUpdating ? todoCancelButton : todoDeleteButton
     );
   }
 
@@ -62,6 +86,32 @@ todoListUl.createTodoElemLi = function (todoItem) {
       ? todoContent.classList.add('completed')
       : todoContent.classList.remove('completed');
   }
+  function handleUpdateButton() {
+    isUpdating = true;
+    todoContent.contentEditable = true;
+    render();
+    todoContent.focus();
+  }
+  function handleSaveButton() {
+    isUpdating = false;
+    todoItem.content = todoContent.innerText;
+    storage.updateTodoItem(todoItem);
+    todoContent.contentEditable = false;
+    render();
+  }
+  function handleDeleteButton() {
+    isUpdating = false;
+    storage.removeTodoItem(todoItem.id);
+    todoElemLi.remove();
+    render();
+  }
+  function handleCancelButton() {
+    isUpdating = false;
+    todoContent.innerText = todoItem.content;
+    todoContent.contentEditable = false;
+    render();
+  }
+
   render();
   todoListUl.prepend(todoElemLi);
 };
